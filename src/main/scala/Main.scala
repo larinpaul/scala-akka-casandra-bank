7331a060528802322b6c658cfc976b83eceb5407
larinpaul <www.larinpavel@mail.ru>
2024-11-10 19:24:29 +0500

    First commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,6 +11,7 @@ object Main {
       //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
       // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
       println(s"i = $i")
+
     }
   }
 }
