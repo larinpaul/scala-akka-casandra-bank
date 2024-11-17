aa95424259db2971b446b22a9534895a17692df3
larinpaul <www.larinpavel@mail.ru>
2024-11-17 17:07:57 +0500

    We can do this...
      sealed trait Command
      object Command {
        case class CreateBankAccount(user: String, currency: String, initialBalance: Double, replyTo: ActorRef[Response]) extends Command
        case class UpdateBalance(id: String, currency: String, amount: Double /* can be < 0*/, replyTo: ActorRef[Response]) extends Command
        case class GetBankAccount(id: String, replyTo: ActorRef[Response]) extends Command
      }
      import Command._

--- src/main/scala/com/rockthejvm/bank/actors/PersistentBankAccount.scala
+++ src/main/scala/com/rockthejvm/bank/actors/PersistentBankAccount.scala
@@ -16,9 +16,12 @@ class PersistentBankAccount {
 
   // commands = messages
   sealed trait Command
-  case class CreateBankAccount(user: String, currency: String, initialBalance: Double, replyTo: ActorRef[Response]) extends Command
-  case class UpdateBalance(id: String, currency: String, amount: Double /* can be < 0*/, replyTo: ActorRef[Response]) extends Command
-  case class GetBankAccount(id: String, replyTo: ActorRef[Response]) extends Command
+  object Command {
+    case class CreateBankAccount(user: String, currency: String, initialBalance: Double, replyTo: ActorRef[Response]) extends Command
+    case class UpdateBalance(id: String, currency: String, amount: Double /* can be < 0*/, replyTo: ActorRef[Response]) extends Command
+    case class GetBankAccount(id: String, replyTo: ActorRef[Response]) extends Command
+  }
+  import Command._
 
   // events = to persist to Cassandra
   trait Event
