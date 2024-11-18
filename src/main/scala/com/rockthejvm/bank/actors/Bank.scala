a232a1d2c9a4170385e396cec0dd9e4b0c14ec5d
larinpaul <www.larinpavel@mail.ru>
2024-11-18 07:52:38 +0500

    Started really creating the Bank actor

--- src/main/scala/com/rockthejvm/bank/actors/Bank.scala
+++ src/main/scala/com/rockthejvm/bank/actors/Bank.scala
@@ -1,11 +1,18 @@
 package com.rockthejvm.bank.actors
 
+import akka.actor.typed.ActorRef
+
 class Bank {
 
   // commands = messages
+  import PersistentBankAccount.Command._
+  import PersistentBankAccount.Command
+
   // events
+  sealed trait Event
+  case class BankAccountCreated(id: String) extends Event
+
   // state
-
-
+  case class State(accounts: Map[String, ActorRef[Command]])
 
 }
