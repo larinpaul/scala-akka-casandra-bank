c8a05a9bc2f13a0b6899af01f6e993c4a1c5a0b2
larinpaul <www.larinpavel@mail.ru>
2024-11-19 07:25:55 +0500

    Added Behaviors.setup into the apply() method

--- src/main/scala/com/rockthejvm/bank/actors/Bank.scala
+++ src/main/scala/com/rockthejvm/bank/actors/Bank.scala
@@ -1,35 +1,37 @@
 package com.rockthejvm.bank.actors
 
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.persistence.typed.PersistenceId
 import akka.persistence.typed.scaladsl.Effect
+import com.rockthejvm.bank.actors.PersistentBankAccount.eventHandler
 
 class Bank {
 
   // commands = messages
+
   import PersistentBankAccount.Command._
   import PersistentBankAccount.Command
 
   // events
   sealed trait Event
+
   case class BankAccountCreated(id: String) extends Event
 
   // state
   case class State(accounts: Map[String, ActorRef[Command]])
 
-  // command handler aka message handler
+  // command handler aka message handler                // run my little function here...
   val commandHandler: (State, Command) => Effect[Event, State] = ???
 
-  // event handler
-  val eventHandler: (State, Event) => State =    ???
-
   // behavior
-  def apply(): Behavior[Command] =
+  def apply(): Behavior[Command] = Behaviors.setup {
+
     EventSourceBehavior[Command, Event, State](
       persistenceId = PersistenceId.ofUniqueId("bank"),
       emptyState = State(Map()),
       commandHandler = commandHandler,
       eventHandler = eventHandler
     )
-
+  }
 }
