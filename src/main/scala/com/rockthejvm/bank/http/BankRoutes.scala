978a7c531bd5fdd26510927cbfaeee716ed357e1
larinpaul <www.larinpavel@mail.ru>
2024-12-11 07:59:52 +0500

    /*     POST /bank       Payload: bank account creation request as JSON       Response:       201 Created       Location: /bank/uuid   */   val routes =     pathPrefix("bank") {       pathEndOrSingleSlash {         post {           // parse the payload         }       }     }

--- src/main/scala/com/rockthejvm/bank/http/BankRoutes.scala
+++ src/main/scala/com/rockthejvm/bank/http/BankRoutes.scala
@@ -1,11 +1,27 @@
 package com.rockthejvm.bank.http
 
+import akka.http.scaladsl.server.Directives._
 import akka.actor.typed.ActorRef
 import com.rockthejvm.bank.actors.PersistentBankAccount.Command
 import com.rockthejvm.bank.actors.PersistentBankAccount.Command._
 
 class BankRoutes(bank: ActorRef[Command]) {
 
+  /*
+    POST /bank
+      Payload: bank account creation request as JSON
+      Response:
+      201 Created
+      Location: /bank/uuid
+  */
+  val routes =
+    pathPrefix("bank") {
+      pathEndOrSingleSlash {
+        post {
+          // parse the payload
+        }
+      }
+    }
 
 
 }
