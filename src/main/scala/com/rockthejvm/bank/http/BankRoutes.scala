51ce4e352ff500f72526afaef3172ae603849a8b
larinpaul <www.larinpavel@mail.ru>
2024-12-13 07:59:33 +0500

    started creating def createBankAccount

--- src/main/scala/com/rockthejvm/bank/http/BankRoutes.scala
+++ src/main/scala/com/rockthejvm/bank/http/BankRoutes.scala
@@ -3,14 +3,19 @@ package com.rockthejvm.bank.http
 import akka.http.scaladsl.server.Directives._
 import akka.actor.typed.ActorRef
 import com.rockthejvm.bank.actors.PersistentBankAccount.Command
+import com.rockthejvm.bank.actors.PersistentBankAccount.Response
 import com.rockthejvm.bank.actors.PersistentBankAccount.Command._
 // Will allow me to use a special directive that will allow to convert a payload to a special case class
 import io.circe.generic.auto._
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 
+import scala.concurrent.Future
+
 case class BankAccountCreationRequest(user: String, balance: Double)
 
 class BankRoutes(bank: ActorRef[Command]) {
+
+  def createBankAccount(request: BankAccountCreationRequest): Future[Response] = ???
 
   /*
     POST /bank
